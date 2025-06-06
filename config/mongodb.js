1b31d9ca49d6017fd58df162db2a538661a9087b
fainnerramirez <fainnerramirez@gmail.com>
2025-06-06 09:59:53 -0500

    refactor: update url

--- config/mongodb.js
+++ config/mongodb.js
@@ -2,8 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try {
-
-        await mongoose.connect('mongodb+srv://fainnerexample:fainner123@cluster-jhon.2vghmgt.mongodb.net/?retryWrites=true&w=majority&appName=Cluster-jhon');
+        await mongoose.connect('mongodb+srv://<user_db>:<password>@cluster-jhon.2vghmgt.mongodb.net/?retryWrites=true&w=majority&appName=Cluster-jhon');
         console.log('Conexión a MongoDB exitosa');
     } catch (err) {
         console.error('Error de conexión a MongoDB', err);
